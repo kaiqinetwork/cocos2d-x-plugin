0773b1f41c609af619d9cc3264673d324baf316a
PerfectXD <PerfectXD@K7-macmini001deMac-mini.local>
2016-10-26 23:18:14 +0800

    *Improved: 友盟插件类名变更 *Improved: 完善ShareSDK插件

--- plugins/sharesdk/proj.ios/ShareSDKShare.m
+++ plugins/sharesdk/proj.ios/ShareSDKShare.m
@@ -23,18 +23,19 @@
  ****************************************************************************/
 
 #import "ShareSDKShare.h"
+#import "ShareWrapper.h"
 
 #import <ShareSDK/ShareSDK.h>
 #import <ShareSDKConnector/ShareSDKConnector.h>
 
-//腾讯开放平台（对应QQ和QQ空间）SDK头文件
+// 腾讯开放平台（对应QQ和QQ空间）SDK头文件
 #import <TencentOpenAPI/TencentOAuth.h>
 #import <TencentOpenAPI/QQApiInterface.h>
 
-//微信SDK头文件
+// 微信SDK头文件
 #import "WXApi.h"
 
-//新浪微博SDK头文件
+// 新浪微博SDK头文件
 #import "WeiboSDK.h"
 
 #define OUTPUT_LOG(...)     if (_debug) NSLog(__VA_ARGS__);
@@ -45,16 +46,108 @@ BOOL _debug;
 
 - (void) configDeveloperInfo: (NSMutableDictionary*) devInfo
 {
-    
+    NSString *appKey = [devInfo objectForKey:@"ShareSDKAppKey"];
+    NSString *sinaWeiboAppKey = [devInfo objectForKey:@"ShareSDKSinaWeiboAppKey"];
+    NSString *sinaWeiboAppSecret = [devInfo objectForKey:@"ShareSDKSinaWeiboAppSecret"];
+    NSString *sinaWeiboRedirectUri = [devInfo objectForKey:@"ShareSDKSinaWeiboRedirectUri"];
+    NSString *weChatAppId = [devInfo objectForKey:@"ShareSDKWeChatAppId"];
+    NSString *weChatAppSecret = [devInfo objectForKey:@"ShareSDKWeChatAppSecret"];
+    NSString *qqAppId = [devInfo objectForKey:@"ShareSDKQQAppId"];
+    NSString *qqAppKey = [devInfo objectForKey:@"ShareSDKQQAppKey"];
+
+    if (appKey == nil) {
+        return;
+    }
+    [ShareSDK registerApp:appKey
+            activePlatforms:@[
+                            @(SSDKPlatformTypeSinaWeibo),
+                            @(SSDKPlatformTypeMail),
+                            @(SSDKPlatformTypeSMS),
+                            @(SSDKPlatformTypeCopy),
+                            @(SSDKPlatformTypeWechat),
+                            @(SSDKPlatformTypeQQ)]
+            onImport:^(SSDKPlatformType platformType)
+            {
+                switch (platformType)
+                {
+                    case SSDKPlatformTypeWechat:
+                        [ShareSDKConnector connectWeChat:[WXApi class]];
+                        break;
+                    case SSDKPlatformTypeQQ:
+                        [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
+                        break;
+                    case SSDKPlatformTypeSinaWeibo:
+                        [ShareSDKConnector connectWeibo:[WeiboSDK class]];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo)
+            {
+         
+                switch (platformType)
+                {
+                    case SSDKPlatformTypeSinaWeibo:
+                        //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
+                        [appInfo SSDKSetupSinaWeiboByAppKey:sinaWeiboAppKey
+                                                  appSecret:sinaWeiboAppSecret
+                                                redirectUri:sinaWeiboRedirectUri
+                                                   authType:SSDKAuthTypeBoth];
+                        break;
+                    case SSDKPlatformTypeWechat:
+                        [appInfo SSDKSetupWeChatByAppId:weChatAppId
+                                              appSecret:weChatAppSecret];
+                        break;
+                    case SSDKPlatformTypeQQ:
+                        [appInfo SSDKSetupQQByAppId:qqAppId
+                                             appKey:qqAppKey
+                                           authType:SSDKAuthTypeBoth];
+                        break;
+                    default:
+                        break;
+                }
+            }];
 }
 
-- (void) share: (NSMutableDictionary*) shareInfo{
+- (void) share: (NSMutableDictionary*) shareInfo
+{
+    NSArray* images;
+    if ([shareInfo objectForKey:@"imagePath"]) {
+        images = @[[UIImage imageNamed:[shareInfo objectForKey:@"imagePath"]]];
+    }
+    NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+    [shareParams SSDKSetupShareParamsByText:[shareInfo objectForKey:@"text"]
+                                     images:images
+                                        url:[NSURL URLWithString:[shareInfo objectForKey:@"url"]]
+                                      title:[shareInfo objectForKey:@"title"]
+                                       type:SSDKContentTypeAuto];
     
+    [ShareSDK share:SSDKPlatformTypeAny
+         parameters:shareParams
+     onStateChanged:^(SSDKResponseState state, NSDictionary *userData, SSDKContentEntity *contentEntity,  NSError *error) {
+                   switch (state) {
+                       case SSDKResponseStateSuccess:
+                       {
+                           [ShareWrapper onShareResult:self withRet:kShareSuccess withMsg:@""];
+                           break;
+                       }
+                       case SSDKResponseStateFail:
+                       {
+                           [ShareWrapper onShareResult:self withRet:kShareFail withMsg:@""];
+                           break;
+                       }
+                       default:
+                           break;
+                   }
+               }
+     ];
 }
 
 - (void) setDebugMode: (BOOL) debug{
     _debug = debug;
 }
+
 - (NSString*) getSDKVersion{
     return @"3.5.0";
 }
